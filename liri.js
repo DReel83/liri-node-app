97a832623c21b6bdc8aa66fabb7f4a3e76484817
David Reel <davidpreel@gmail.com>
2016-08-27 16:28:53 -0400

    added spotify to liri

--- liri.js
+++ liri.js
@@ -49,3 +49,34 @@ function movie(){
 			console.log('-----------------------------------------');
 		}
 }
+
+// Spotify function to search for song info.
+function spotify(){
+	var search = process.argv[2];
+
+ 
+spotify.search({ type: 'track', query: search }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+    else{
+    	var songData =  data.tracks.items[0];
+    	//console.log(songData);
+
+    	var artist = songData.artists[0].name;
+    	var name = songData.name;
+    	var album = songData.album.name;
+    	var preview = songData.preview_url;
+
+
+    	console.log("Name: " + name + "\nArtist/s: " + artist + "\nAlbum: " + album + "\nPreview URL: " + preview);
+    }	  
+
+
+});
+
+// Twitter function, grabs recents tweets
+function twitter(){
+	
+}
